d242b5badb70661bed8931169ecb0c5dbe52a20c
Cihan <cihankurt1998@gmail.com>
2018-05-08 15:55:25 +0200

    V2
    Mergesort Poging, fail.

--- Sprint 1/code/main.cpp
+++ Sprint 1/code/main.cpp
@@ -3,81 +3,69 @@
 #include "FileStructure.h"
 #include "Key.h"
 
-
-int Compare(const std::string a, const std::string b) //Van groot naar klein.
-{
-  if (a < b)
-  {
-    return 1;
-  }
-  else if (a > b)
-  {
-    return 0;
-  }
-  else
-  {
-    return -1;
-  }
-}
-
-void Swap(Value* current, Value* temp)
-{
-  temp = current->getPrev();
-  current->setPrev(current->getPrev()->getPrev());
-  temp->setPrev(current);
-}
-
-void Sort_Values(Value* head) //bubblesort
+int getLength(Key* head)
 {
   int count = 0;
-  int i;
-  Value *start = head;
-  Value *curr = NULL;
-  Value *trail = NULL;
-  Value *temp = NULL;
-
-
-  while (start->getPrev() != NULL)
-  { //grab count
+  Key* temp = head;
+  while (temp != NULL)
+  {
     count++;
-    start = start->getPrev();
+    temp = temp->getPrev();
   }
-
-  for (i = 0; i < count; ++i)
-  { //for every element in the list
-
-    curr = start;
-    trail = start; //set curr and trail at the start node
-   // std::cout << curr->getText(); //TESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSST
-    while (curr= NULL)
-    { //for the rest of the elements in the list
-      std::cout << "hua";
-      if (Compare(curr->getText(), curr->getPrev()->getText()) == 1)
-      { //compare curr and curr->next
-        std::cout << "hoi";
-        Swap(curr, temp);
-
-        //now we need to setup pointers for trail and possibly head
-        if (curr == head)
-        {
-          std::cout << "doei";
-          head = temp;
-          trail = temp;
-        } //this is the case of the first element swapping to preserve the head pointer
-
-        else //setup trail correctly
-          trail->setPrev(temp);
-        curr = temp; //update curr to be temp since the positions changed
-      }
-      //advance pointers
-      trail = curr;
-      curr = curr->getPrev();
-    }
-  }
+  return count;
 }
 
+Key* MergeList(Key* a, Key* b) {
+    Key* result = NULL;
+    if (a == NULL)
+    {
+      return b;
+    }
+    if (b == NULL)
+    {
+      return a;
+    }
+    
+    if (a->getText() > b->getText()) 
+    {
+      result = b;
+      result->setPrev(MergeList(a, b->getPrev()));
+    } 
+    else 
+    {
+      result = a;
+      result->setPrev( MergeList(a->getPrev(), b));
+    }
+    return result;
+}
 
+Key* MergeSort(Key* head)
+{
+    Key* oldHead = head;
+    // find the length of the linkedlist
+    int mid = getLength(head) / 2;
+    if (head->getPrev() == NULL)
+    {
+      return head;
+    }
+    // set one pointer to the beginning of the list and another at the next
+    // element after mid
+    while (mid - 1 > 0)
+    {
+      oldHead = oldHead->getPrev();
+      mid--;
+    }
 
+    Key* newHead = oldHead->getPrev();// make newHead points to the beginning of
+                  // the second half of the list
+    oldHead->setPrev(NULL);// break the list
+    oldHead = head;// make one pointer points at the beginning of the first
+          // half of the list
+    Key* t1 = MergeSort(oldHead);//make recursive calls 
+    Key* t2 = MergeSort(newHead);
+return MergeList(t1, t2); // merge the sorted lists
+
+}
 
 int main()
 {
@@ -89,6 +77,9 @@ int main()
   k.addValue("eik");
   k.addValue("einsteinium");
   k.addValue("eik");
+  k.addValue("ei");
+  k.addValue("eieren");
+  k.addValue("eigendom");
   k.addValue("taxibaan");
   k.addValue("tafel");
   k.addValue("calculator");
@@ -96,11 +87,13 @@ int main()
   // next line is only to show what kind of data we're working with
   // remove this line to increase performance!
   //head.print();
-  k.print();
+  k.print(); 
+  std::cout << std::endl;
 
   // sort all data
   // todo: call your sort method(s) here!
-  Sort_Values(k.getValuePtr());
+  //std::cout << k.getValuePtr()->getPrev()->getPrev()->getPrev()->getText();
+  MergeSort(&k);
   k.print();
 
   // save sorted data into a new file called sorted.bin
